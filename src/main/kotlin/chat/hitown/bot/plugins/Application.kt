90eae23a9c73e408941fd10dd3e605bb64b356f2
Nyirongo2000 <bed-com-21-19@unima.ac.mw>
2025-03-03 18:07:47 +0200

    Fix compilation errors and dependency issues

--- src/main/kotlin/chat/hitown/bot/plugins/Application.kt
+++ src/main/kotlin/chat/hitown/bot/plugins/Application.kt
@@ -15,7 +15,7 @@ import io.ktor.server.routing.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.plugins.defaultheaders.*
 import ch.qos.logback.classic.Level
 
